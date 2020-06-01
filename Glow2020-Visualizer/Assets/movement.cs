c940a8d76597a604b07112ac14926042b7ba5503
Vincent V <vincentvenhuizen1997@gmail.com>
2020-06-01 13:50:45 +0200

    Fixed movement script and added more controls & Build
    Now able to adjust camera position using J,K,L and I.
    Able to set camera positioning speed using O and P
    Set aura density a bit lower.
    Build the .exe with new fixes

--- Glow2020-Visualizer/Assets/movement.cs
+++ Glow2020-Visualizer/Assets/movement.cs
@@ -9,6 +9,8 @@ public class movement : MonoBehaviour
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+
+    public float speedMultiplier = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,23 @@ public class movement : MonoBehaviour
     {
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        if (Input.GetKey("j")){
+            yaw = yaw - 10 * speedMultiplier;
+        } else if (Input.GetKey("l")){
+            yaw = yaw + 10 * speedMultiplier;
+        }
+        if (Input.GetKey("i")){
+            pitch = pitch - 10 * speedMultiplier;
+        } else if (Input.GetKey("k")){
+            pitch = pitch + 10 * speedMultiplier;
+        }
+
+        if (Input.GetKey("o")){
+            speedMultiplier = speedMultiplier + 0.25f;
+        } else if (Input.GetKey("p")){
+            speedMultiplier = speedMultiplier - 0.25f;
+        }
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
